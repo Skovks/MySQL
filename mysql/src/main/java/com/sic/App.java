e232f7268ece32028f906baaf84890b97eb35a82
skovks <al2152000114@azc.uam.mx>
2021-10-15 19:29:26 -0500

    Conexion a base de datos
    Se compilo el codigo

--- mysql/src/main/java/com/sic/App.java
+++ mysql/src/main/java/com/sic/App.java
@@ -1,13 +1,41 @@
-package com.sic;
+package com.sic; //instruccion que indefica el programa en el arquetipo maven
+
+import java.sql.*; //instruccion que importa la biblioteca
 
 /**
- * Hello world!
+ * Programa que permite comunicarse con una base de datos desde java
+ * 
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+        
+        //intento de accion en este programa
+        try {
+            //codigo del try
+            Class.forName("com.mysql.jdbc.Driver"); //Haciendo uso del conector de java
+            //conexion a base de datos, 4 parametros: Servicio de datos, nombre de usuario, contrasena 
+            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/datos", "skovks17", "osiris117");
+            
+            //Nuevo statement para llamada de datos
+            Statement stmt = con.createStatement(); 
+            //Creación de Query | llamada a todos los datos de la tabla alumnos 
+            ResultSet rs = stmt.executeQuery("select * from alumnos");
+            //Ciclo de todos los elementos obtenidos por el query 
+            while (rs.next()) //
+            //Impresión de los valores 
+            System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3)+ " " + rs.getString(4)); 
+            //Se cierra la conexión  
+            con.close(); 
+        }
+        catch (Exception e){
+            //codigo del catch
+            //Imprimir errores  
+            System.out.println(e); 
+        }
+
     }
 }
